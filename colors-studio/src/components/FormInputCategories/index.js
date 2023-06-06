bad41b6b00aff468801ecee1e82980301723849b
Eduardo <duardohenriqe19@gmail.com>
2023-06-06 13:17:46 -0300

    Revert "categorias disabled"
    This reverts commit 67595b8b35100558dd978d8e2c234d7b1098992e.

--- colors-studio/src/components/FormInputCategories/index.js
+++ colors-studio/src/components/FormInputCategories/index.js
@@ -8,7 +8,7 @@ const InputSelect = (props) => {
             <label className='text-white text-2xl font-light mb-3'>{props.label}</label>
 
             <select id="category" value={props.value} onChange={props.onChange} name="category" className='text-white py-5 px-3 text-lg bg-input rounded border-none outline-none shadow'>
-                        <option disabled selected>Selecione uma das categorias abaixo:</option>
+                        <option>Selecione uma das categorias abaixo:</option>
                         <option>Drill</option>
                         <option className='text-white hover:text-orange-600 transition-all'>Boombap</option>
                         <option>R&B</option>
