6f39ddba251c3c46d1a4577f91d328fae0aeb16d
Eduardo <duardohenriqe19@gmail.com>
2023-06-06 14:15:11 -0300

    scroll to top

--- colors-studio/src/App.js
+++ colors-studio/src/App.js
@@ -1,13 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NewVideo from './pages/NewVideo';
 import NotFound from './pages/NotFound';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+     });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
+    <ScrollToTop/>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
